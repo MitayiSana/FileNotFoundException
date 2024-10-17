dd4626647e3cc487bbfa3d910872e7657b42c3f7
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:45:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace FileNotFoundExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string filePath = "C://abc.txt";
+                File.ReadAllText(filePath);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("Couldn't found the file");
+            }
+            
+            
+            
+        }
+    }
+}
